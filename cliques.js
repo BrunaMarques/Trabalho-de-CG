a6c18910a412431575e951945f1e83a1652cbbe7
Bruna <bruna99stefany@gmail.com>
2020-01-31 23:35:06 -0300

    adicionando firulas

--- cliques.js
+++ cliques.js
@@ -102,6 +102,7 @@ function setTranslacao() {
   if (temselect == false) {
     alert("Selecione um objeto primeiro!");
   } else {
+    alert("Selecione dois pontos");
     n = 2;
     cordTranf = [];
     op = "translacao";
@@ -168,9 +169,9 @@ function setAjuda() {
 
 function getInput() {
   var linhaComando = document.getElementById("linhacomando").value;
-};
+}
 
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", function(event) {
   if (event.ctrlKey && event.key === "z") {
     listaDesenho.pop();
     desenhaLista();
